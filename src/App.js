2f1c10d7352e7647a2ed1620c3ea42cf8c6f4735
hetaljoshi2425 <hetaljoshi2425@gmail.com>
2023-10-24 16:43:36 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with hetal
+          Learn React
         </a>
       </header>
     </div>
